edfa6edcf9e053a09376e8a2f664979e65b51260
RajYadav75 <rajyadav751811@gmail.com>
2025-08-13 23:27:43 +0530

    CRUD API Created

---
+++ 08-MiniProject-CRUD Application/CRUD-API/src/test/java/in/raj/crudapi/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.raj.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
