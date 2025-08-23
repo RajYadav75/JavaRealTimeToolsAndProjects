12fd1b6d476f8ac07c70596aca69f1c8f98bd9a5
RajYadav75 <rajyadav751811@gmail.com>
2025-08-23 23:50:47 +0530

    hql query method implemented in repository interface

--- 10-MiniProject-Report Generation Application/Report-Generation-API/src/main/java/in/raj/repo/ICourseDetailsRepository.java
+++ 10-MiniProject-Report Generation Application/Report-Generation-API/src/main/java/in/raj/repo/ICourseDetailsRepository.java
@@ -2,6 +2,19 @@ package in.raj.repo;
 
 import in.raj.entity.CourseDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
 
 public interface ICourseDetailsRepository extends JpaRepository<CourseDetails, Integer> {
+    @Query("select  distinct(courseCategory)  from  CourseDetails")
+    public  Set<String>    getUniqueCourseCategories();
+
+    @Query("select  distinct(facultyName)  from  CourseDetails")
+    public  Set<String>    getUniqueFacultyNames();
+
+    @Query("select  distinct(trainingMode)  from  CourseDetails")
+    public default Set<String> getUniqueTraniningModes() {
+        return null;
+    }
 }
